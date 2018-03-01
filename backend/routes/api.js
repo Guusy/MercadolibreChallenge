3035780bb6d7cf903c3236b13dcf1215877104cc
Guusy <guusygonzalo@gmail.com>
2018-03-01 00:30:06 -0300

    /api/items?q=:query in express implemented

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const requestPromise = require('request-promise');
 
 router.get('/items', function (req, res) {
-    res.json('live')
+    const item = req.query.q
+    console.log(item)
+    if(item ===undefined){
+        res.status(400).json("Bad Request")
+    }else{
+        requestPromise(`https://api.mercadolibre.com/sites/MLA/search?q=${item}`)
+        .then((result) => {
+           res.status(200).json(JSON.parse(result));
+       })
+       .catch(e=>res.status(503).json(e))
+    }
+    
 });
 router.get('/items/:id', function (req, res) {
     res.json('live')
